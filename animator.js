3f809771227dfde79bd7b705253be11cfc6cf542
Anthony-Petrov <167039870+Anthony-Petrov@users.noreply.github.com>
2025-01-26 16:36:19 -0800

    Update animator.js

--- animator.js
+++ animator.js
@@ -1,29 +1,32 @@
 class Animator {
     constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration) {
-        Object.assign(this, {spritesheet, xStart, yStart, width, height, frameCount, frameDuration});
+        Object.assign(this, { spritesheet, xStart, yStart, width, height, frameCount, frameDuration });
 
         this.elapsedTime = 0;
         this.totalTime = frameCount * frameDuration;
-    };
+    }
 
     drawFrame(tick, ctx, x, y) {
-
         this.elapsedTime += tick;
-        const frame = this.currentFrame();
 
-        
+        // **Ensure animation resets after full cycle**
+        if (this.elapsedTime >= this.totalTime) {
+            this.elapsedTime = 0; // **This ensures looping**
+        }
+
+        const frame = this.currentFrame();
         ctx.drawImage(this.spritesheet,
             this.xStart + this.width * frame, this.yStart,
             this.width, this.height,
             x, y,
             this.width, this.height);
-    };
+    }
 
-    currentFrame(){
-        return Math.floor(this.elapsedTime / this.frameDuration);
-    };
+    currentFrame() {
+        return Math.floor(this.elapsedTime / this.frameDuration) % this.frameCount; // Ensures looping
+    }
 
-    isDone(){
-        return (this.elapsedTime >= this.totalTime);
-    };
+    isDone() {
+        return this.elapsedTime >= this.totalTime;
+    }
 }
